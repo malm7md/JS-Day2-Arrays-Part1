8a38c15dbba3ed3e36e41b42b475d078027d169f
malm7md <malm7md20091@gmail.com>
2024-08-05 20:56:19 +0300

    functions.js

---
+++ arrays1.js
@@ -0,0 +1,16 @@
+let groceries = [ "icecream" , "milk" , "chocolate", "candy" , "fries" ]
+groceries.pop()
+//console.log(groceries.slice(1,4))
+ groceries.push("cheese" , "toast")
+
+
+console.log(groceries);
+console.log(groceries.length);
+
+groceries.splice(2,1);
+console.log(groceries);
+
+groceries.unshift("cola")
+console.log(groceries);
+
+
